1e741b8d08ca94327f9814be0841a9e966d50748
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-12-01 16:44:32 -0500

    allow a query for whether the extension is installed

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -2,9 +2,11 @@
 
 function receiveMessage(event) {
   switch (event.data.name) {
+  case "cineScreenShareCheckForExtension":
+    window.postMessage({ name: "cineScreenShareHasExtension" }, "*")
+    break
   case "cineScreenShare":
     chrome.runtime.sendMessage({ action: "getScreen" }, function(id) {
-      console.log("callback id:", id)
       window.postMessage({ name: "cineScreenShareResponse", id: id }, "*")
     })
     break
@@ -16,15 +18,11 @@ function receiveMessage(event) {
 
 function notifyHasExtension() {
   window.postMessage({name: "cineScreenShareHasExtension"}, "*");
-  var hasExtensionNode = document.createElement('div')
-  hasExtensionNode.id = 'cine-screenshare-has-extension'
-  hasExtensionNode.style.display = 'none'
-  document.body.appendChild(hasExtensionNode)
 }
 
 function main() {
   window.addEventListener("message", receiveMessage, false);
-  notifyHasExtension()
+  window.postMessage({name: "cineScreenShareHasExtension"}, "*");
 }
 
 main()
